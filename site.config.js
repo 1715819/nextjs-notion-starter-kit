f591277e7d8cef5ebb009de499886d9bf20b9303
1715819 <75056973+1715819@users.noreply.github.com>
2022-04-05 20:15:36 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 export default {
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '92127893323f419ead9cf540b8cd812e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '藏剑的空间',
+  domain: 'blog.wyxyj.com',
+  author: '藏剑',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
